bd44eeab90dfeef0af9b2f5219acba18424fe74c
max <maximilian.nasarewski@googlemail.com>
2012-06-03 19:17:40 +0100

    removed user query to b.php

--- b.php
+++ b.php
@@ -1,3 +1,11 @@
 <?
 	$c = mysql_connect("localhost","root","");
 	mysql_select_db("fb_db",$c);
+	//fetch user data and accign them to vars
+	$r = mysql_query("select * from user",$c);
+	while($row = mysql_fetch_assoc($r)){
+		$uid = $row['id'];
+		$name['first'] =$row['fname'];
+		$name['last'] = $row['sname'];
+	}
+	
--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?
 	include("b.php");
-	$r = mysql_query("select * from user",$c);
-	while($row = mysql_fetch_assoc($r)){
-		$uid = $row['id'];
-		$name['first'] =$row['fname'];
-		$name['last'] = $row['sname'];
-	}
 	include("h.php");
 	include("le.php");
 	include("m.php");
