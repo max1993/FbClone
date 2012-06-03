d3b82285b194528586699dcc64e4f410fee9fbbf
max <maximilian.nasarewski@googlemail.com>
2012-06-03 18:46:54 +0100

    setup base structure

---
+++ index.php
@@ -0,0 +1,10 @@
+<?
+	//mysql_connect("localhost","root","","fb_db");
+	//$r=mysql_query("select * from user");
+	include(l.php;);
+	include(h.php;);
+	include(le.php;);
+	include(m.php;);
+	include(r.php;);
+	include(f.php;);
+?>
